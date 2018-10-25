0288b54991768b6dcf0278939c8cfe7e6dc5e7a5
Kim Seonghyun <kim.seonghyun@ericsson.com>
2018-10-25 14:58:11 +0200

    Remove unused testcase
    Signed-off-by: Kim Seonghyun <kim.seonghyun@ericsson.com>

--- tests/testthat/test-cbar.R
+++ tests/testthat/test-cbar.R
@@ -17,24 +17,3 @@ test_that("cbar", {
   expect_error(cbar(.data, 1:100000, 1:10000))
 })
 
-test_that("cbar", {
-  .data <- iris[, 1:4]
-  datetime <- seq(from = Sys.time(), length.out = nrow(.data), by = "mins")
-  .data <- cbind(datetime = datetime, .data)
-
-  pre_period <- 1:100
-  post_period <- 101:150
-
-  set.seed(1)
-  res_1 <- cbar(.data, pre_period, post_period, seed = 1, nseasons = 96)
-  expect_true(inherits(res_1, "cbar"))
-
-  set.seed(1)
-  res_2 <- cbar(.data, pre_period, post_period, seed = 1)
-  expect_true(inherits(res_2, "cbar"))
-
-  expect_error(cbar(.data, -3:-1, -100:-20))
-  expect_error(cbar(.data, 1:100000, 1:10000))
-})
-
-
